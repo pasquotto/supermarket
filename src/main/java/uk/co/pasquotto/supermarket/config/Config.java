bf4e906665f1dc3429d37ecec4b0275495dced31
Rafael Costa <pasquotto@gmail.com>
2018-03-11 23:04:07 +0000

    getting rule file from configuration

--- src/main/java/uk/co/pasquotto/supermarket/config/Config.java
+++ src/main/java/uk/co/pasquotto/supermarket/config/Config.java
@@ -4,20 +4,33 @@ import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
 import org.kie.api.builder.KieModule;
+import org.kie.api.io.Resource;
 import org.kie.api.runtime.KieContainer;
 import org.kie.internal.io.ResourceFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
 
+    @Value("${rules.file:file.drl}")
+    private String ruleFile;
+
+    @Value("${rules.file.fromClasspath:false}")
+    private boolean fileFromClasspath = false;
+
     @Bean
     public KieContainer kieContainer() {
         KieServices kieServices = KieServices.Factory.get();
-
+        Resource resource = null;
+        if(fileFromClasspath) {
+            resource = ResourceFactory.newClassPathResource(ruleFile);
+        } else {
+            resource = ResourceFactory.newFileResource(ruleFile);
+        }
         KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
-        kieFileSystem.write(ResourceFactory.newClassPathResource("file.drl"));
+        kieFileSystem.write(resource);
         KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
         kieBuilder.buildAll();
         KieModule kieModule = kieBuilder.getKieModule();
